6b0ad90bd5fb28b7c891429cd4e215a7aced0bea
jenny <cikwaninejenny.com>
2022-09-16 20:01:24 +0100

    Les evenements dans react et changement de state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,47 @@ import Car from "./components/Car";
 import "./App.css";
 
 class App extends Component {
-  state = { 
-    titre: "Mon catalogue vooture"
-   } 
-  render() { 
+  state = {
+    titre: "Mon catalogue voiture",
+  };
+
+  changeTitle = (e) => {
+    console.log(e.target);
+    this.setState({
+      titre: "Mon nouveau titre",
+    });
+  };
+
+  changeViaParam = (titre) => {
+    this.setState({
+      titre: titre,
+    });
+  };
+
+  changeViaBind = (param) => {
+    this.setState({
+      titre: param,
+    });
+  };
+
+  changeViaInput = (e)=> {
+    this.setState({
+      titre: e.target.value
+    })
+  }
+
+  render() {
     return (
-      <div className= "App">
+      <div className="App">
         <Mycar title={this.state.titre} />
+        <button onClick={this.changeTitle}>Changer en dur</button>
+        <button onClick={() => this.changeViaParam("Titre via un parm")}>
+          Via param
+        </button>
+        <button onClick={this.changeViaBind.bind(this, "Titre via Bind")}>
+          Via Bind
+        </button>
+        <input type="text" onChange={this.changeViaInput} value={this.state.titre}  />
       </div>
     );
   }
--- src/components/Mycar.jsx
+++ src/components/Mycar.jsx
@@ -2,14 +2,21 @@
  import Car from "./Car";
 
 class Mycar extends Component {
+
+  noCopy = () => {
+    alert("merci de ne pas copier le texte");
+  }
+
   render () {
     console.log(this)
 
      return (
        <div>
-         <h1> {this.props.title} </h1>
+         <h1 > {this.props.title} </h1>
+         
+         <p onCopy={this.noCopy}>Lorem ipsum dolor sit amet consectetur adipisicing elit</p> 
 
-         <Car color='red'>Rang Rover</Car>
+         <Car color='red'>R ang Rover</Car>
          <Car>Mercedes</Car>
          <Car color='green'></Car>
 
@@ -18,6 +25,6 @@ class Mycar extends Component {
      );
   }
 }
-
+  
 export default Mycar;
 
